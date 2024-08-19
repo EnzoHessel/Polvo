047af67f7ebb4a370df3641e3eecc678a91e3c48
Enzo Hessel <enzo.carmo@salg.com.br>
2024-08-19 11:59:23 -0300

    chore: add hero component and fix logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="flex justify-between gap-8">
       <div className="max-w-40 flex flex-col gap-8">
-        <h2>
+        <h2 className="text-xl">
           Ninguém inova sozinho. Do Agro ao Varejo. De Fintech a Anytech. A maneira como desenvolvemos software é única.
         </h2>
         <Button variant="default" size="default">
@@ -16,7 +16,7 @@ const Hero = () => {
         <Image src="/hero.svg" alt="Hero" width={616} height={452} className="object-cover"/>
       </div>
       <div className="max-w-40">
-        <p>Unleashing the <span className="text-secondary">Future_</span> of Innovation. <br/><br/> <span className="text-secondary">#We are Polvo</span></p>
+        <p className="text-2xl">Unleashing the <span className="text-secondary">Future_</span> of Innovation. <br/><br/> <span className="text-secondary">#We are Polvo</span></p>
       </div>
     </section>
   )
