c2961855d25d4eec4e7eb4ed425d5f1ac729d04c
Enzo Hessel Araujo do Carmo <enzohessel12342@gmail.com>
2025-01-15 09:17:28 -0300

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
           {children}
+          {/* <Footer */}
       </body>
     </html>
   );
