22805e22be686799d1867aa8b634fb44eb836ffb
Enzo Hessel Araujo do Carmo <enzohessel12342@gmail.com>
2025-01-19 13:51:46 -0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
+        <Analytics />
         <Header />
           {children}
         <Footer />
