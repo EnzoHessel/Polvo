2f30c753288f19f81de3586f6a50c0177bdae9e4
Enzo Hessel Araujo do Carmo <enzohessel12342@gmail.com>
2025-01-19 13:57:56 -0300

    fix: speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <Analytics />
+        <SpeedInsights />
         <Header />
           {children}
         <Footer />
