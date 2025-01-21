5b02f283a5a85e05eaf6a94d4045f915653b4933
Enzo Hessel Araujo do Carmo <enzohessel12342@gmail.com>
2025-01-21 11:15:17 -0300

    fix: duplicate modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      automaticNameDelimiter: '-',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
