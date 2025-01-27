ab4a306fe4923869689363e0d8f4d4b85556c37d
Enzo Hessel Araujo do Carmo <enzohessel12342@gmail.com>
2025-01-27 11:24:57 -0300

    refactor: resposive hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
-import { motion } from 'framer-motion';
 import { Button } from './ui/button';
 import styles from '@/styles/hero.module.css';
 import Link from 'next/link';
@@ -11,64 +10,52 @@ const Hero = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <motion.section
-      className={styles.hero}
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <div className={styles.heroText}>
-        <div className={styles.heroTitle}>
-          <h2>
-            Ninguém inova sozinho.
-            <br />
-            <br />
-            A maneira como desenvolvemos software é única.
-          </h2>
-        </div>
-        <Button variant="default" size="default">
-          <Link href="https://api.whatsapp.com/send?phone=5511996232675&text=Ol%C3%A1,%20vim%20pela%20Polvo%20e%20gostaria%20de%20um%20or%C3%A7amento%20personalizado." target='_blank'>
-            COMEÇAR AGORA
-          </Link>
-        </Button>
-      </div>
+    <section className={styles.hero}>
 
-      <div
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className={styles.imageContainer}
-      >
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: isHovered ? 0 : 1 }}
-          transition={{ duration: 0.5 }}
-          className={styles.image}
+      <div className={styles.heroContent}>
+        <div className={styles.heroText}>
+          <div className={styles.heroTitle}>
+            <h2>
+              Ninguém inova sozinho.
+              <br />
+              <br />
+              A maneira como desenvolvemos software é única.
+            </h2>
+          </div>
+          <Button variant="default" size="default">
+            <Link href="https://api.whatsapp.com/send?phone=5511996232675&text=Ol%C3%A1,%20vim%20pela%20Polvo%20e%20gostaria%20de%20um%20or%C3%A7amento%20personalizado." target='_blank'>
+              COMEÇAR AGORA
+            </Link>
+          </Button>
+        </div>
+
+        <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className={styles.imageContainer}
         >
-          <Image
-            src="/hero.svg"
-            alt="Hero"
-            fill
-            style={{ objectFit: 'cover' }}
-            priority
-            draggable="false"
-          />
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: isHovered ? 1 : 0 }}
-          transition={{ duration: 0.5 }}
-          className={styles.image}
-        >
-          <Image
-            src="/hero-2.svg"
-            alt="Hero Colorido"
-            fill
-            style={{ objectFit: 'cover' }}
-            quality={100}
-            priority
-            draggable="false"
-          />
-        </motion.div>
+          <div className={styles.image} style={{ display: isHovered ? 'none' : 'block' }}>
+            <Image
+              src="/hero.svg"
+              alt="Hero"
+              fill
+              style={{ objectFit: 'cover' }}
+              priority
+              draggable="false"
+            />
+          </div>
+          <div className={styles.image} style={{ display: isHovered ? 'block' : 'none' }}>
+            <Image
+              src="/hero-2.svg"
+              alt="Hero Colorido"
+              fill
+              style={{ objectFit: 'cover' }}
+              quality={100}
+              priority
+              draggable="false"
+            />
+          </div>
+        </div>
       </div>
 
       <div className={styles.heroFooter}>
@@ -76,7 +63,7 @@ const Hero = () => {
           Unleashing the <span className="text-secondary">Future_</span> of Innovation. <br /><br /> <span className="text-secondary">#We are Polvo</span>
         </p>
       </div>
-    </motion.section>
+    </section>
   );
 };
 
